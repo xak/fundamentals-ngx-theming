a2d62c579833c633ca3f157ac2b4b10839d75a47
Zack <424119+xak@users.noreply.github.com>
2019-01-08 13:44:42 -0800

    guess number game setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { AlertComponent } from './alert/alert.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  maxnumber: number;
+  deviation: number;
+  noOfTries: number;
+  original: number;
+  guess: number;
+  message: string;
+  success: boolean;
+
+  constructor() {
+    this.initalizeGame();
+  }
+  initalizeGame() {
+      this.noOfTries = 0;
+      this.maxnumber = 10;
+      this.original = Math.floor((Math.random() * this.maxnumber) + 1);
+      this.guess = null;
+      this.deviation = null;
+      this.message = null;
+      this.success = false;
+  }
+  verifyGuess() {
+      this.deviation = this.original - this.guess;
+      this.noOfTries = this.noOfTries + 1;
+      if (this.deviation < 0) {
+        this.message = "Your guess is higher"
+
+      } else if (this.deviation > 0) {
+        this.message = "Your guess is lower";
+      } else {
+        this.success = true;
+        this.message = null;
+      }
+
+  }
+
 }
